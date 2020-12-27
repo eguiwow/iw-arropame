b0cbc11c549a869b20690bc9127a8d1ff0618896
Erlantz <erlantzmarcos@opendeusto.es>
2020-12-27 20:26:06 +0100

    done

--- elresorthSop/elresorthSop/appelresortShop/urls.py
+++ elresorthSop/elresorthSop/appelresortShop/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('producto/<int:item_id>/', views.producto, name = 'producto'),
     path('carrito/', views.carrito, name = 'carrito'),
     path('compra/', views.compra, name = 'compra'),
-    path('carrito/<int:producto_id>/', views.carrito_anyadir, name = 'carrito_anyadir'), # esta url es para probar que podemos guardar al carrito bien habría que hacerlo directamente con js desde 'producto.html'
+    path('carrito/?id=<int:producto_id>/', views.carrito_anyadir, name = 'carrito_anyadir'), # esta url es para probar que podemos guardar al carrito bien habría que hacerlo directamente con js desde 'producto.html'
     path('<str:genero>/<str:tipo>/', views.articulos, name = 'articulos'), # El ultimo por que sino coge por defecto el str:genero como cualquier cadena que se meta
 ]
